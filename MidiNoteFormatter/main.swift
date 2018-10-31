1f83c23b1356940b20a641b43c42d1963e376745
Nathaniel Day <nathan_day@mac.com>
2018-10-31 22:40:26 +1300

    Created.

--- MidiNoteFormatter/main.swift
+++ MidiNoteFormatter/main.swift
@@ -8,5 +8,20 @@
 
 import Foundation
 
-print("Hello, World!")
+let		theMidiNoteFormatter = MidiNoteFormatter();
+theMidiNoteFormatter.accentStyle = [.sharp];
 
+print("Midi number ot note name");
+for m in 60...71 {
+	print( "midi # \(m) = \(theMidiNoteFormatter.string(forMidiNote:m))");
+}
+
+print("\nNote name to Midi number");
+for n in ["C3", "C♯3", "D3", "D♯3", "E3", "F3", "F♯3", "G3", "G♯3", "A3", "A♯3", "B3"] {
+	let		r = theMidiNoteFormatter.midiNoteValue(for:n);
+	if let m = r.value {
+		print( "note \(n) = \(m)");
+	} else {
+		print( "error = \(r.error!)");
+	}
+}
