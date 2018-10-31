be23ac7e03dbc7234724147137a584d09359c0fb
Nathaniel Day <nathan_day@mac.com>
2018-10-31 20:56:32 +1300

    Initial Commit

---
+++ MidiNoteFormatter/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MidiNoteFormatter
+//
+//  Created by Nathaniel Day on 31/10/18.
+//  Copyright © 2018 Nathaniel Day. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
