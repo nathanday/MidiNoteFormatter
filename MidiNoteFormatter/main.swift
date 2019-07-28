74c816e436f3ddc735a1ec01524d75e4164ed953
Nathaniel Day <nathan_day@mac.com>
2019-07-28 22:41:55 +1000

    Added tests

--- MidiNoteFormatter/main.swift
+++ MidiNoteFormatter/main.swift
@@ -25,3 +25,13 @@ for n in ["C3", "C♯3", "D3", "D♯3", "E3", "F3", "F♯3", "G3", "G♯3", "A3"
 		print( "error = \(r.error!)");
 	}
 }
+
+
+for (i,n) in ["C3", "C♯3", "D3", "D♯3", "E3", "F3", "F♯3", "G3", "G♯3", "A3", "A♯3", "B3", "C4", "C♯4", "D4", "D♯4", "E4", "F4", "F♯4", "G4", "G♯4", "A4", "A♯4", "B4"].enumerated() {
+  assert( theMidiNoteFormatter.string(forMidiNote:(48+i)) == n )
+  assert( theMidiNoteFormatter.midiNoteValue(for:n).value == 48+i )
+}
+
+for m in 0...127 {
+  assert( theMidiNoteFormatter.midiNoteValue(for:theMidiNoteFormatter.string(forMidiNote:m)).value == m )
+}
